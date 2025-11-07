ebab9f59d40126958bfaa683378ac21dc1bf7984
Abhishek Choudhary <abhishekchoudhary38808@gmail.com>
2025-11-07 22:47:29 +0530

    Update the API_BASE path in config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const API_BASE = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
-
+export const API_BASE = process.env.REACT_APP_API_URL || "https://chat-backend-ec08.onrender.com/api";
